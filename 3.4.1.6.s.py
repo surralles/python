268bceae2faee5512c1c0b3b89c964d179d86edb
Jordi Surrallés <surralles@gmail.com>
2025-12-17 12:16:13 +0100

    Add files via upload

---
+++ 3.4.1.6.s.py
@@ -0,0 +1,19 @@
+﻿hat_list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+]  # Esta es una lista existente de números ocultos en el sombrero.
+
+# Paso 1
+hat_list[2] = int(input("Ingresa un número entero: "))
+
+# Paso 2
+del hat_list[-1]
+
+# Paso 3
+print(len(hat_list))
+
+
+print(hat_list)
